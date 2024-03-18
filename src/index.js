50af9910c4d1e48d9a51e0d8834c040f2adf6989
brunost2 <bruno.santos421@gmail.com>
2024-03-18 13:54:48 -0300

    att

--- src/index.js
+++ src/index.js
@@ -20,7 +20,16 @@ app.delete("/:id", async (req, res) =>{
   const film = await Film.findByIdAndDelete(req.params.id)
   return res.send(film)
 })
+ app.put ("/: id", async (req, res) =>{
+  const film = await Film.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url
+  })
 
+  return res.send(film)
+ })
 app.post("/", async (req, res) => {
 const film = new Film ({
   title: req.body.title,
